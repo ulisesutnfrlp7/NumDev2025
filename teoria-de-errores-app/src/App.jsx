00038adb5ccc62e3f52433a95352a1ef00a9f6e2
Ulises Mateo Bucchino <ulibucchino@gmail.com>
2025-08-30 22:52:41 -0300

    archivos

--- teoria-de-errores-app/src/App.jsx
+++ teoria-de-errores-app/src/App.jsx
@@ -1,19 +1,19 @@
 // src/App.jsx
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./apps/HomeApp";
 import Practica from "./apps/PracticeApp";
 import TheoryApp from "./apps/TheoryApp";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/teoria" element={<TheoryApp />} /> 
         <Route path="/practica" element={<Practica />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
