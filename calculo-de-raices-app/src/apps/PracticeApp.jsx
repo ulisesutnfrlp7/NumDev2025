6b3320d37964e5577509da8cf5470c0ff33afcd7
Soy tomi <tomasgregorio2005@gmail.com>
2025-09-09 20:13:16 -0300

    Agregro la calculadora del metodo de Newton

--- calculo-de-raices-app/src/apps/PracticeApp.jsx
+++ calculo-de-raices-app/src/apps/PracticeApp.jsx
@@ -1,30 +1,26 @@
 // src/apps/PracticeApp.jsx
 
+
+
 import { useNavigate } from 'react-router-dom';
+import NewtonMethodCalculator from '../components/NewtonMethodCalculator';
 
 function PracticeApp() {
-  const navigate = useNavigate();
-
-  return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-200 to-indigo-400 p-6 flex flex-col items-center justify-center relative text-gray-800">
-      <button
-        onClick={() => navigate("/")}
-        className="absolute top-6 left-6 px-6 py-3 bg-blue-600 text-white rounded-xl text-lg font-semibold hover:bg-blue-700 hover:scale-105 transition-transform shadow-md"
-      >
-        ⬅ Volver al Home
-      </button>
-
-      <h1 className="text-3xl md:text-4xl font-bold mb-8 text-center">
-        Análisis Numérico — UNIDAD 6: CÁLCULO DE RAÍCES
-      </h1>
-
-      <div className="bg-white p-6 rounded-2xl shadow-lg max-w-2xl w-full text-center">
-        <p className="text-lg text-gray-700">
-          Aquí irá el contenido práctico: simuladores, inputs, resultados, etc.
-        </p>
-      </div>
-    </div>
-  );
-}
-
+    const navigate = useNavigate();
+    return (
+        <div className="app">
+            <button
+                onClick={() => navigate("/")}
+                className="mt-8 px-6 py-3 bg-green-600 text-white rounded-xl text-lg font-semibold hover:bg-green-700 transition"
+                style={{ position: "absolute", top: "100px", left: "40px" }}
+            >
+                ⬅ Volver al Home
+            </button>
+            <h1 className="mt-20 mb-8 text-3xl font-bold text-green-700 text-center drop-shadow-sm tracking-wide">
+                Análisis Numérico - <span className="text-green-800">UNIDAD 6:</span> <span className="text-green-600">CÁLCULO DE RAÍCES</span>
+            </h1>
+            <NewtonMethodCalculator />
+        </div>
+    );
+};
 export default PracticeApp;
